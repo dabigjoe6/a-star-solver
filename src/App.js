8cc1de5ff0c910d5b63550e94232eced08e38648
Joseph <dabigjoe6@gmail.com>
2020-05-13 08:16:12 +0100

    Render dumm graph

--- src/App.js
+++ src/App.js
@@ -1,50 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import Graph from "react-graph-vis";
 import './App.css';
 
 function App() {
+
   const graph = {
-    nodes: [
-      { id: 1, label: "Node 1", title: "node 1 tootip text" },
-      { id: 2, label: "Node 2", title: "node 2 tootip text" },
-      { id: 3, label: "Node 3", title: "node 3 tootip text" },
-      { id: 4, label: "Node 4", title: "node 4 tootip text" },
-      { id: 5, label: "Node 5", title: "node 5 tootip text" }
-    ],
-    edges: [
-      { from: 1, to: 2 },
-      { from: 1, to: 3 },
-      { from: 2, to: 4 },
-      { from: 2, to: 5 }
-    ]
+	nodes: [{"id":0,"label":"0","x":78.01603911549438,"y":49.474860768712915,"title":"Node 0"},{"id":1,"label":"1","x":52.49831588690298,"y":14.953665513987202,"title":"Node 1"},{"id":2,"label":"2","x":80.85335344099086,"y":76.96330846542071,"title":"Node 2"},{"id":3,"label":"3","x":25.99134798656856,"y":14.485659826020548,"title":"Node 3"},{"id":4,"label":"4","x":73.53838928272886,"y":80.89961609345659,"title":"Node 4"},{"id":5,"label":"5","x":9.088671576431507,"y":72.22846879290788,"title":"Node 5"},{"id":6,"label":"6","x":31.399901818675602,"y":1.876171413125327,"title":"Node 6"},{"id":7,"label":"7","x":68.24813442515915,"y":80.16111783687677,"title":"Node 7"},{"id":8,"label":"8","x":20.128789391122524,"y":43.196344222361226,"title":"Node 8"},{"id":9,"label":"9","x":85.51947714242674,"y":90.11339078096633,"title":"Node 9"},{"id":10,"label":"10","x":75.8173658978441,"y":24.026772497187533,"title":"Node 10"},{"id":11,"label":"11","x":25.311953895059137,"y":10.321622277398102,"title":"Node 11"},{"id":12,"label":"12","x":48.13859169876731,"y":50.06237737207431,"title":"Node 12"},{"id":13,"label":"13","x":91.12422509614865,"y":18.390287606062962,"title":"Node 13"},{"id":14,"label":"14","x":4.580558670435442,"y":58.86703168399895,"title":"Node 14"},{"id":15,"label":"15","x":45.825231730833075,"y":17.355062674618672,"title":"Node 15"},{"id":16,"label":"16","x":12.939557977525574,"y":69.00163281403961,"title":"Node 16"},{"id":17,"label":"17","x":60.7698913404794,"y":36.2322730884702,"title":"Node 17"},{"id":18,"label":"18","x":71.9569201584275,"y":13.985272363426526,"title":"Node 18"},{"id":19,"label":"19","x":88.60336256842245,"y":89.1868301175821,"title":"Node 19"},{"id":20,"label":"20","x":42.38357358399233,"y":2.6771817842421997,"title":"Node 20"},{"id":21,"label":"21","x":82.52497121120051,"y":95.32681441921305,"title":"Node 21"},{"id":22,"label":"22","x":47.41500928703473,"y":73.53428557575755,"title":"Node 22"},{"id":23,"label":"23","x":26.253385360950578,"y":97.68234503830939,"title":"Node 23"},{"id":24,"label":"24","x":93.63713903322149,"y":13.022993020357044,"title":"Node 24"},{"id":25,"label":"25","x":62.43437191127234,"y":21.665962402659545,"title":"Node 25"},{"id":26,"label":"26","x":55.72917679006295,"y":20.83567880838434,"title":"Node 26"},{"id":27,"label":"27","x":74.82655725962591,"y":12.631654071213482,"title":"Node 27"},{"id":28,"label":"28","x":64.35799740880603,"y":54.88515965193208,"title":"Node 28"},{"id":29,"label":"29","x":34.50980271391931,"y":88.00306496459869,"title":"Node 29"},{"id":30,"label":"30","x":2.1423673670808885,"y":46.66482714834408,"title":"Node 30"},{"id":31,"label":"31","x":64.0952694324525,"y":32.327114125080655,"title":"Node 31"},{"id":32,"label":"32","x":17.44020534279049,"y":95.28527425842739,"title":"Node 32"},{"id":33,"label":"33","x":13.32965908314021,"y":39.965106417431976,"title":"Node 33"},{"id":34,"label":"34","x":58.3993110207876,"y":42.704536740474666,"title":"Node 34"},{"id":35,"label":"35","x":30.738657277050628,"y":9.186645974288632,"title":"Node 35"},{"id":36,"label":"36","x":74.0625863119245,"y":68.128520136847,"title":"Node 36"},{"id":37,"label":"37","x":33.45284735051981,"y":65.69436279895382,"title":"Node 37"},{"id":38,"label":"38","x":17.972981733780145,"y":99.9395685828547,"title":"Node 38"},{"id":39,"label":"39","x":63.15322816286787,"y":73.11657634689946,"title":"Node 39"}],
+	edges: [{"from":0,"to":36},{"from":0,"to":34},{"from":0,"to":31},{"from":0,"to":28},{"from":0,"to":17},{"from":1,"to":35},{"from":1,"to":31},{"from":1,"to":27},{"from":1,"to":26},{"from":1,"to":25},{"from":1,"to":20},{"from":1,"to":18},{"from":1,"to":17},{"from":1,"to":15},{"from":1,"to":6},{"from":2,"to":39},{"from":2,"to":36},{"from":2,"to":21},{"from":2,"to":19},{"from":2,"to":9},{"from":2,"to":7},{"from":2,"to":4},{"from":3,"to":35},{"from":3,"to":20},{"from":3,"to":15},{"from":3,"to":11},{"from":3,"to":6},{"from":4,"to":39},{"from":4,"to":36},{"from":4,"to":21},{"from":4,"to":19},{"from":4,"to":9},{"from":4,"to":7},{"from":4,"to":2},{"from":5,"to":32},{"from":5,"to":16},{"from":5,"to":14},{"from":6,"to":35},{"from":6,"to":20},{"from":6,"to":15},{"from":6,"to":11},{"from":6,"to":1},{"from":6,"to":3},{"from":7,"to":39},{"from":7,"to":36},{"from":7,"to":22},{"from":7,"to":21},{"from":7,"to":19},{"from":7,"to":9},{"from":7,"to":2},{"from":7,"to":4},{"from":8,"to":33},{"from":8,"to":30},{"from":8,"to":14},{"from":9,"to":36},{"from":9,"to":21},{"from":9,"to":19},{"from":9,"to":2},{"from":9,"to":4},{"from":9,"to":7},{"from":10,"to":31},{"from":10,"to":27},{"from":10,"to":26},{"from":10,"to":25},{"from":10,"to":24},{"from":10,"to":18},{"from":10,"to":17},{"from":10,"to":13},{"from":11,"to":35},{"from":11,"to":20},{"from":11,"to":15},{"from":11,"to":3},{"from":11,"to":6},{"from":12,"to":37},{"from":12,"to":34},{"from":12,"to":31},{"from":12,"to":28},{"from":12,"to":22},{"from":12,"to":17},{"from":13,"to":27},{"from":13,"to":24},{"from":13,"to":18},{"from":13,"to":10},{"from":14,"to":33},{"from":14,"to":30},{"from":14,"to":16},{"from":14,"to":5},{"from":14,"to":8},{"from":15,"to":35},{"from":15,"to":31},{"from":15,"to":26},{"from":15,"to":25},{"from":15,"to":20},{"from":15,"to":17},{"from":15,"to":1},{"from":15,"to":3},{"from":15,"to":6},{"from":15,"to":11},{"from":16,"to":37},{"from":16,"to":30},{"from":16,"to":5},{"from":16,"to":14},{"from":17,"to":34},{"from":17,"to":31},{"from":17,"to":28},{"from":17,"to":26},{"from":17,"to":25},{"from":17,"to":18},{"from":17,"to":0},{"from":17,"to":1},{"from":17,"to":10},{"from":17,"to":12},{"from":17,"to":15},{"from":18,"to":31},{"from":18,"to":27},{"from":18,"to":26},{"from":18,"to":25},{"from":18,"to":24},{"from":18,"to":1},{"from":18,"to":10},{"from":18,"to":13},{"from":18,"to":17},{"from":19,"to":21},{"from":19,"to":2},{"from":19,"to":4},{"from":19,"to":7},{"from":19,"to":9},{"from":20,"to":35},{"from":20,"to":26},{"from":20,"to":1},{"from":20,"to":3},{"from":20,"to":6},{"from":20,"to":11},{"from":20,"to":15},{"from":21,"to":2},{"from":21,"to":4},{"from":21,"to":7},{"from":21,"to":9},{"from":21,"to":19},{"from":22,"to":39},{"from":22,"to":37},{"from":22,"to":29},{"from":22,"to":7},{"from":22,"to":12},{"from":23,"to":38},{"from":23,"to":32},{"from":23,"to":29},{"from":24,"to":27},{"from":24,"to":10},{"from":24,"to":13},{"from":24,"to":18},{"from":25,"to":34},{"from":25,"to":31},{"from":25,"to":27},{"from":25,"to":26},{"from":25,"to":1},{"from":25,"to":10},{"from":25,"to":15},{"from":25,"to":17},{"from":25,"to":18},{"from":26,"to":34},{"from":26,"to":31},{"from":26,"to":27},{"from":26,"to":1},{"from":26,"to":10},{"from":26,"to":15},{"from":26,"to":17},{"from":26,"to":18},{"from":26,"to":20},{"from":26,"to":25},{"from":27,"to":31},{"from":27,"to":1},{"from":27,"to":10},{"from":27,"to":13},{"from":27,"to":18},{"from":27,"to":24},{"from":27,"to":25},{"from":27,"to":26},{"from":28,"to":39},{"from":28,"to":36},{"from":28,"to":34},{"from":28,"to":31},{"from":28,"to":0},{"from":28,"to":12},{"from":28,"to":17},{"from":29,"to":38},{"from":29,"to":37},{"from":29,"to":32},{"from":29,"to":22},{"from":29,"to":23},{"from":30,"to":33},{"from":30,"to":8},{"from":30,"to":14},{"from":30,"to":16},{"from":31,"to":34},{"from":31,"to":0},{"from":31,"to":1},{"from":31,"to":10},{"from":31,"to":12},{"from":31,"to":15},{"from":31,"to":17},{"from":31,"to":18},{"from":31,"to":25},{"from":31,"to":26},{"from":31,"to":27},{"from":31,"to":28},{"from":32,"to":38},{"from":32,"to":5},{"from":32,"to":23},{"from":32,"to":29},{"from":33,"to":8},{"from":33,"to":14},{"from":33,"to":30},{"from":34,"to":0},{"from":34,"to":12},{"from":34,"to":17},{"from":34,"to":25},{"from":34,"to":26},{"from":34,"to":28},{"from":34,"to":31},{"from":35,"to":1},{"from":35,"to":3},{"from":35,"to":6},{"from":35,"to":11},{"from":35,"to":15},{"from":35,"to":20},{"from":36,"to":39},{"from":36,"to":0},{"from":36,"to":2},{"from":36,"to":4},{"from":36,"to":7},{"from":36,"to":9},{"from":36,"to":28},{"from":37,"to":12},{"from":37,"to":16},{"from":37,"to":22},{"from":37,"to":29},{"from":38,"to":23},{"from":38,"to":29},{"from":38,"to":32},{"from":39,"to":2},{"from":39,"to":4},{"from":39,"to":7},{"from":39,"to":22},{"from":39,"to":28},{"from":39,"to":36}]
   };
+
+
  
   const options = {
-    layout: {
-      hierarchical: true
-    },
-    edges: {
-      color: "#000000"
-    },
-    height: "500px"
+	edges: {
+	  color: "#000000"
+	},
   };
  
   const events = {
-    select: function(event) {
-      var { nodes, edges } = event;
-    }
+	select: function(event) {
+	  var { nodes, edges } = event;
+	}
   };
   return (
-    <Graph
-      graph={graph}
-      options={options}
-      events={events}
+	<Graph
+	  graph={graph}
+	  options={options}
+	  events={events}
 	  style={{ height: '100vh' }}
-      getNetwork={network => {
-        //  if you want access to vis.js network api you can set the state in a parent component using this property
-      }}
-    />
+	  getNetwork={network => {
+		//  if you want access to vis.js network api you can set the state in a parent component using this property
+	  }}
+	/>
   );
 }
 
