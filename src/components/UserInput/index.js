0ac214b8c884c64dd1e82b38cc3625af020d7134
Joseph <dabigjoe6@gmail.com>
2020-05-14 01:23:38 +0100

    User input styles

--- src/components/UserInput/index.js
+++ src/components/UserInput/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-
+import './styles.css'
 export default function UserInput({ startNode, endNode, setStartNode, setEndNode }) {
 	return (
-		<div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-		<div style={{ display: 'flex', alignSelf: 'center', justifyContent: 'space-around', alignItems: 'center', maxWidth: '50vw' }}>
-			<div style={{ margin: 10, display: 'flex', alignItems: 'center' }}>
-				<p style={{ marginRight: 10 }}>Start Node: </p>
-				<input type="text" value={startNode} onChange={(value) => setStartNode(Number(value))} />
+		<div className="user-input-container">
+			<div className="user-input-wrapper">
+				<div className="user-text-input"  >
+					<p className="user-input-label">Start Node: </p>
+					<input type="text" value={startNode} onChange={(value) => setStartNode(Number(value))} />
+				</div>
+				<div className="user-text-input" >
+					<p className="user-input-label">Goal Node: </p>
+					<input type="text" value={endNode} onChange={(value) => setEndNode(Number(value))} />
+				</div>
 			</div>
-			<div style={{ margin: 10, display: 'flex', alignItems: 'center' }}>
-				<p style={{ marginRight: 10 }}>Goal Node: </p>
-				<input type="text" value={endNode} onChange={(value) => setEndNode(Number(value))} />
-			</div>
-		</div>
 		</div>
 	)
 } 
